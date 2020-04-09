df4a48f0a0c6758b2cf96f3744cc5e5c53af42c5
erick <erickandela_rocker@hotmail.com>
2020-04-09 12:40:46 -0500

    Responses_Directly

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -10,8 +10,10 @@
   plug :assign_welcome_message, "Welcome" when action in [:index, :show]
   def index(conn, _params) do
     conn
-    |> assign(:message, "Welcome Forward")
-    |> render("index.html")
+    |> put_resp_content_type("text/plain")
+    |> send_resp(201, "")
+    # |> assign(:message, "Welcome Forward")
+    # |> render("index.html")
   end
 
   defp assign_welcome_message(conn, msg)do
